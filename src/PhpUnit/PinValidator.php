c2d1c6cfa9d76a0b03bb3e922102bfbd9484584e
Jakub Frajt <jakub.frajt@gmail.com>
2018-11-11 17:46:06 +0100

    PHPUnit - PinValidator

---
+++ src/PhpUnit/PinValidator.php
@@ -0,0 +1,49 @@
+<?php /** @noinspection NotOptimalRegularExpressionsInspection */
+
+declare(strict_types=1);
+
+namespace App\PhpUnit;
+
+class PinValidator
+{
+    private const LENGTH = 4;
+
+    private const DISALLOWED_COMBINATIONS = [
+        '1234', '4321'
+    ];
+
+    /**
+     * @var array
+     */
+    private $errors = [];
+
+    /**
+     * @param string $value
+     *
+     * @return array errors
+     */
+    public function validate(string $value): array
+    {
+        if ($value === '') {
+            $this->errors[] = 'PIN cannot be empty.';
+
+            return $this->errors;
+        }
+
+        if (\strlen($value) !== self::LENGTH) {
+            $this->errors[] = 'PIN must contains exactly four numbers.';
+
+            return $this->errors;
+        }
+
+        if (preg_match('/\D/', $value)) {
+            $this->errors[] = 'PIN must contains only numbers';
+        }
+
+        if (\in_array($value, self::DISALLOWED_COMBINATIONS, true)) {
+            $this->errors[] = 'This combination is not allowed.';
+        }
+
+        return $this->errors;
+    }
+}
---
+++ tests/PhpUnit/PinValidatorTest.php
@@ -0,0 +1,81 @@
+<?php
+declare(strict_types=1);
+
+use App\PhpUnit\PinValidator;
+use App\PhpUnit\ValidationException;
+use PHPUnit\Framework\TestCase;
+
+class PinValidatorTest extends TestCase
+{
+    /**
+     * @var PinValidator
+     */
+    private $validator;
+
+    public function setUp()
+    {
+        $this->validator = new PinValidator();
+    }
+
+    /**
+     * @dataProvider validPinProvider
+     */
+    public function testValidPin(string $value): void
+    {
+       $this->assertEmpty($this->validator->validate($value));
+    }
+
+    /**
+     * @dataProvider invalidPinProvider
+     */
+    public function testInvalidPin(string $value): void
+    {
+        $this->assertGreaterThan(0, $this->validator->validate($value));
+    }
+
+    /**
+     * @dataProvider invalidCombinationsProvider
+     */
+    public function testInvalidCombinations(string $value): void
+    {
+        $this->assertEquals(['This combination is not allowed.'], $this->validator->validate($value));
+    }
+
+    public function testInvalidLength(): void
+    {
+        $this->assertEquals(['PIN must contains exactly four numbers.'], $this->validator->validate('23'));
+    }
+
+    public function testNotNumericPin(): void
+    {
+        $this->assertEquals(['PIN must contains only numbers'], $this->validator->validate('12a5'));
+    }
+
+    public function validPinProvider(): array
+    {
+        return [
+            ['4479'],
+            ['4513'],
+            ['9761'],
+            ['0036'],
+        ];
+    }
+
+    public function invalidPinProvider(): array
+    {
+        return [
+            ['abab'],
+            ['12as'],
+            ['AAAA'],
+            ['126?'],
+        ];
+    }
+
+    public function invalidCombinationsProvider(): array
+    {
+        return [
+            ['1234'],
+            ['4321'],
+        ];
+    }
+}
