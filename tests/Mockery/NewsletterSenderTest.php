5477b0dc9823e31a8d8e58b8dfebbd80f14ee091
Jakub Frajt <jakub.frajt@gmail.com>
2018-11-18 15:38:40 +0100

    Mockery - NewsletterSenderTest

---
+++ tests/Mockery/NewsletterSenderTest.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Mockery;
+
+use App\DTO\User;
+use App\MailerInterface;
+use App\NewsletterSender;
+use App\UsersRepository;
+use Mockery as m;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+class NewsletterSenderTest extends MockeryTestCase
+{
+    public function testSendEmailWhenWeDontHaveActiveUsers(): void
+    {
+        $usersRepositoryStub = m::mock(UsersRepository::class);
+        $usersRepositoryStub
+            ->allows()
+            ->getActiveUsers()
+            ->andReturns([]);
+
+        $mailerSpyMock = m::spy(MailerInterface::class);
+
+        $newsletterSender = new NewsletterSender($usersRepositoryStub, $mailerSpyMock);
+        $newsletterSender->sendNewsletterToActiveUsers();
+
+        $mailerSpyMock->shouldNotHaveReceived('send');
+    }
+
+    public function testSendEmail(): void
+    {
+        $usersRepositoryStub = m::mock(UsersRepository::class);
+        $usersRepositoryStub
+            ->allows()
+            ->getActiveUsers()
+            ->andReturns([
+                $this->createUserWithEmail('jimmy@example.com'),
+                $this->createUserWithEmail('mathew@example.com'),
+                $this->createUserWithEmail('tony@example.com'),
+            ]);
+
+        $mailerSpyMock = m::spy(MailerInterface::class);
+
+        $newsletterSender = new NewsletterSender($usersRepositoryStub, $mailerSpyMock);
+        $newsletterSender->sendNewsletterToActiveUsers();
+
+        $mailerSpyMock->shouldHaveReceived('send')->times(3);
+    }
+
+    private function createUserWithEmail(string $email): User
+    {
+        return new User(1, 'test', 'test', $email, true);
+    }
+}
