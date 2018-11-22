99e41ab4a9352ce0fbc3fc8a671045e7e0a49c70
Jakub Frajt <jakub.frajt@gmail.com>
2018-11-22 09:19:11 +0100

    Codeception - NewsletterSenderTest

---
+++ tests/Codeception/NewsletterSenderTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Codeception;
+
+use App\DTO\User;
+use App\MailerInterface;
+use App\NewsletterSender;
+use App\UsersRepository;
+use Codeception\Stub;
+use Codeception\Stub\Expected;
+
+class NewsletterSenderTest extends \Codeception\Test\Unit
+{
+    public function testSendEmailWhenWeDontHaveActiveUsers(): void
+    {
+        $usersRepositoryStub = Stub::make(UsersRepository::class, ['getActiveUsers' => []]);
+
+        $mailerMock = $this->makeEmpty(MailerInterface::class, [
+            'send' => Expected::never()
+        ]);
+
+        $newsletterSender = new NewsletterSender($usersRepositoryStub, $mailerMock);
+        $newsletterSender->sendNewsletterToActiveUsers();
+    }
+
+    public function testSendEmail(): void
+    {
+        $usersRepositoryStub = Stub::make(UsersRepository::class, [
+            'getActiveUsers' => [
+                $this->createUserWithEmail('jimmy@example.com'),
+                $this->createUserWithEmail('mathew@example.com'),
+                $this->createUserWithEmail('tony@example.com'),
+            ]
+        ]);
+
+        $mailerMock = $this->makeEmpty(MailerInterface::class, [
+            'send' => Expected::exactly(3)
+        ]);
+
+        $newsletterSender = new NewsletterSender($usersRepositoryStub, $mailerMock);
+        $newsletterSender->sendNewsletterToActiveUsers();
+    }
+
+    private function createUserWithEmail(string $email): User
+    {
+        return new User(1, 'test', 'test', $email, true);
+    }
+}
