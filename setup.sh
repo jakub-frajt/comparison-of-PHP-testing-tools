5d01fba7fcfdb0414eeaa5b5c60bbb3c6e2e89ad
Jakub Frajt <jakub.frajt@gmail.com>
2018-11-23 16:28:01 +0100

    adding to setup.sh installing dependencies via Composer

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo 'Installing dependecies via Composer ...'
+docker run --rm -it -v $PWD:/app -u $(id -u):$(id -g) composer install;
+
 echo 'Building Docker environment...'
 docker-compose build
 
