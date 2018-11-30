7625b3d802ecafbefa598aea31b34b54f894aa98
Jakub Frajt <mail@jakubfrajt.cz>
2018-11-30 14:07:16 +0100

    update of a script for setup, adding var directory to git

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 echo 'Installing dependecies via Composer ...'
-docker run --rm -it -v $PWD:/app -u $(id -u):$(id -g) composer install;
+docker run --rm -it -v $PWD:/app -u $(id -u):$(id -g) composer install
 
-echo 'Building Docker environment...'
-docker-compose build
-
-echo 'Starting Docker containers ...'
+echo 'Building && starting Docker containers ...'
 docker-compose up -d
+
+chmod -R 777 var/
